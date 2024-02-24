d7130ec57348b9f4572cff77ee55f69f05b9dc00
DanielMcKeeAlpha <daniel.mckee@alphasights.com>
2024-02-24 18:01:27 +0000

    Add tabs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,38 @@
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
-import { AgGridReact } from 'ag-grid-react'; // AG Grid Component
-import 'ag-grid-community/styles/ag-grid.css'; // Mandatory CSS required by the grid
-import 'ag-grid-community/styles/ag-theme-quartz.css';
-import { useEffect, useState } from 'react';
-import { ColDef, Grid } from 'ag-grid-community';
-import { x } from '@xstyled/styled-components';
-import { BaselineTable } from '@/components/BaselineTable/BaselineTable';
-import { BidSheetTable } from '@/components/BidSheetTableTable/BidSheetTable';
-import { BidSheetRow } from '@/types/bidSheet.types';
-import { BaselineRow } from '@/types/baseline.types';
-import { CurrentItemTable } from '@/components/CurrentItemTable/CurrentItemTable';
-import { CurrentItem } from '@/types/currentItem.types';
-import { SelectedItem } from '@/components/SelectedItem/SelectedItem';
-import { FileUpload } from '@mui/icons-material';
-import { UploadForm } from '@/components/UploadFile/UploadFile';
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { AgGridReact } from "ag-grid-react"; // AG Grid Component
+import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
+import "ag-grid-community/styles/ag-theme-quartz.css";
+import { useEffect, useState } from "react";
+import { ColDef, Grid } from "ag-grid-community";
+import { x } from "@xstyled/styled-components";
+import { BaselineTable } from "@/components/BaselineTable/BaselineTable";
+import { BidSheetTable } from "@/components/BidSheetTableTable/BidSheetTable";
+import { BidSheetRow } from "@/types/bidSheet.types";
+import { BaselineRow } from "@/types/baseline.types";
+import { CurrentItemTable } from "@/components/CurrentItemTable/CurrentItemTable";
+import { CurrentItem } from "@/types/currentItem.types";
+import { SelectedItem } from "@/components/SelectedItem/SelectedItem";
+import { FileUpload } from "@mui/icons-material";
+import { UploadForm } from "@/components/UploadFile/UploadFile";
+import { BuyingTabs } from "@/components/BuyingTabs/BuyingTabs";
 // Create new GridExample component
 const GridExample = () => {};
 export default function Home() {
   const [rowData, setRowData] = useState<CurrentItem[]>([]); // [1
 
   const bidSheet = useQuery<BidSheetRow[]>({
-    queryKey: ['bidSheet'],
+    queryKey: ["bidSheet"],
     queryFn: async () => {
-      const response = await axios.get('/api/bid-sheet');
+      const response = await axios.get("/api/bid-sheet");
       return response.data;
     },
   });
 
   const baseline = useQuery<BaselineRow[]>({
-    queryKey: ['baseline'],
+    queryKey: ["baseline"],
     queryFn: async () => {
-      const response = await axios.get('/api/baseline');
+      const response = await axios.get("/api/baseline");
       return response.data;
     },
   });
@@ -40,7 +41,8 @@ export default function Home() {
 
   return (
     <x.div h="100vh" p="1.5rem">
-      <UploadForm />
+      <BuyingTabs />
+      {/* <UploadForm />
       <CurrentItemTable
         rowData={rowData}
         setRowData={setRowData}
@@ -55,7 +57,7 @@ export default function Home() {
         selectedRow={selectedRow}
       />
       <BaselineTable rowData={baseline.data ?? []} />
-      <BidSheetTable rowData={bidSheet.data ?? []} />
+      <BidSheetTable rowData={bidSheet.data ?? []} /> */}
     </x.div>
   );
 }
